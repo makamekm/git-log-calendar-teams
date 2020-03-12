492276e67814c4b3222c22539656c72227024ab1
makame <makame@mail.ru>
2020-03-12 16:00:38 +0100

    add hints

--- index.js
+++ index.js
@@ -1,124 +1,151 @@
+const Repo = require('git-tools');
 const fs = require('fs');
 const d3Node = require('d3-node');
 const d3n = new d3Node();
-const d3 = d3n.d3
+const d3 = d3n.d3;
+
+const repo = new Repo('./');
+// repo.authors((error, authors) => {
+//   if (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+
+//   console.log(authors);
+// });
 
 let data = require('./data');
-data = data.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+data = [];
 
-let minYear = 100000;
-let maxYear = 0;
+//  --author="\(Adam\)\|\(Jon\)"
 
-const checkYear = (date) => {
-  const year = date.getFullYear();
-
-  if (year > maxYear) {
-    maxYear = year;
+repo.activeDays('master', function(error, activeDays) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
   }
 
-  if (year < minYear) {
-    minYear = year;
+  console.log(activeDays);
+  for (let key in activeDays.dates) {
+    data.push({ Date: key, Value: activeDays.dates[key] });
   }
-}
 
-const dateValues = data.map(dv => {
-  const date = new Date(dv.Date);
-  checkYear(date);
-  return {
-    date: d3.timeDay(date),
-    value: Number(dv.Value)
+  data = data.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+
+  let minYear = 100000;
+  let maxYear = 0;
+
+  const checkYear = date => {
+    const year = date.getFullYear();
+
+    if (year > maxYear) {
+      maxYear = year;
+    }
+
+    if (year < minYear) {
+      minYear = year;
+    }
   };
+
+  const dateValues = data.map(dv => {
+    const date = new Date(dv.Date);
+    checkYear(date);
+    return {
+      date: d3.timeDay(date),
+      value: Number(dv.Value)
+    };
+  });
+
+  const yearGap = Math.abs(maxYear - minYear);
+
+  const cellSize = 15;
+  const yearHeight = cellSize * 7;
+  const marginTop = cellSize * 0.5;
+  const marginLeft = 50;
+  const marginRight = 10;
+  const weeksInYear = 53;
+
+  const [width, height] = [marginLeft + cellSize * (weeksInYear + 1) + marginRight, yearHeight * (yearGap + 1) + 2 * marginTop];
+  const svg = d3n.createSVG(width, height);
+
+  const years = d3
+    .nest()
+    .key(d => d.date.getUTCFullYear())
+    .entries(dateValues)
+    .reverse();
+
+  const values = dateValues.map(c => c.value);
+  const maxValue = d3.max(values);
+  const minValue = d3.min(values);
+
+  const group = svg.append('g');
+
+  const year = group
+    .selectAll('g')
+    .data(years)
+    .join('g')
+    .attr('transform', (d, i) => `translate(${marginLeft}, ${yearHeight * i + marginTop})`);
+
+  year
+    .append('text')
+    .attr('x', -5)
+    .attr('y', -30)
+    .attr('text-anchor', 'end')
+    .attr('font-size', 16)
+    .attr('font-weight', 550)
+    .attr('font-family', 'Arial')
+    .attr('transform', 'rotate(270)')
+    .text(d => d.key);
+
+  const formatDay = d => ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'][d.getUTCDay()];
+  const countDay = d => d.getUTCDay();
+  const timeWeek = d3.utcSunday;
+  const formatDate = d3.utcFormat('%x');
+  const colorFn = d3.scaleSequential(d3.interpolateBuGn).domain([Math.floor(minValue), Math.ceil(maxValue)]);
+  const format = d3.format('+.2%');
+
+  year
+    .append('g')
+    .attr('text-anchor', 'end')
+    .selectAll('text')
+    .data(d3.range(7).map(i => new Date(1995, 0, i)))
+    .join('text')
+    .attr('x', -5)
+    .attr('y', d => (countDay(d) + 0.5) * cellSize)
+    .attr('dy', '0.31em')
+    .attr('font-size', 12)
+    .text(formatDay);
+
+  year
+    .append('g')
+    .selectAll('rect')
+    .data(d => d.values)
+    .join('rect')
+    .attr('width', cellSize - 1.5)
+    .attr('height', cellSize - 1.5)
+    .attr('x', (d, i) => timeWeek.count(d3.utcYear(d.date), d.date) * cellSize + 10)
+    .attr('y', d => countDay(d.date) * cellSize + 0.5)
+    .attr('fill', d => colorFn(d.value))
+    .append('title')
+    .text(d => `${formatDate(d.date)}: ${d.value.toFixed(2)}`);
+
+  // const legend = group.append('g').attr('transform', `translate(10, ${years.length * yearHeight + cellSize * 4})`);
+
+  // const categoriesCount = 10;
+  // const categories = [...Array(categoriesCount)].map((_, i) => {
+  //   const upperBound = (maxValue / categoriesCount) * (i + 1);
+  //   const lowerBound = (maxValue / categoriesCount) * i;
+
+  //   return {
+  //     upperBound,
+  //     lowerBound,
+  //     color: d3.interpolateBuGn(upperBound / maxValue),
+  //     selected: true
+  //   };
+  // });
+
+  fs.writeFileSync('output.svg', d3n.svgString());
 });
-
-const yearGap = Math.abs(maxYear - minYear);
-
-const cellSize = 15;
-const yearHeight = cellSize * 7;
-const marginTop = cellSize * 0.5;
-const marginLeft = 50;
-const marginRight = 10;
-const weeksInYear = 53;
-
-const [ width, height ] = [marginLeft + cellSize * (weeksInYear + 1) + marginRight, yearHeight * (yearGap + 1) + 2 * marginTop];
-const svg = d3n.createSVG(width, height);
-
-const years = d3
-  .nest()
-  .key(d => d.date.getUTCFullYear())
-  .entries(dateValues)
-  .reverse();
-
-const values = dateValues.map(c => c.value);
-const maxValue = d3.max(values);
-const minValue = d3.min(values);
-
-const group = svg.append('g');
-
-const year = group
-  .selectAll('g')
-  .data(years)
-  .join('g')
-  .attr('transform', (d, i) => `translate(${marginLeft}, ${yearHeight * i + marginTop})`);
-
-year
-  .append('text')
-  .attr('x', -5)
-  .attr('y', -30)
-  .attr('text-anchor', 'end')
-  .attr('font-size', 16)
-  .attr('font-weight', 550)
-  .attr('font-family', 'Arial')
-  .attr('transform', 'rotate(270)')
-  .text(d => d.key);
-
-const formatDay = d => ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'][d.getUTCDay()];
-const countDay = d => d.getUTCDay();
-const timeWeek = d3.utcSunday;
-const formatDate = d3.utcFormat('%x');
-const colorFn = d3.scaleSequential(d3.interpolateBuGn).domain([Math.floor(minValue), Math.ceil(maxValue)]);
-const format = d3.format('+.2%');
-
-year
-  .append('g')
-  .attr('text-anchor', 'end')
-  .selectAll('text')
-  .data(d3.range(7).map(i => new Date(1995, 0, i)))
-  .join('text')
-  .attr('x', -5)
-  .attr('y', d => (countDay(d) + 0.5) * cellSize)
-  .attr('dy', '0.31em')
-  .attr('font-size', 12)
-  .text(formatDay);
-
-year
-  .append('g')
-  .selectAll('rect')
-  .data(d => d.values)
-  .join('rect')
-  .attr('width', cellSize - 1.5)
-  .attr('height', cellSize - 1.5)
-  .attr('x', (d, i) => timeWeek.count(d3.utcYear(d.date), d.date) * cellSize + 10)
-  .attr('y', d => countDay(d.date) * cellSize + 0.5)
-  .attr('fill', d => colorFn(d.value))
-  .append('title')
-  .text(d => `${formatDate(d.date)}: ${d.value.toFixed(2)}`);
-
-// const legend = group.append('g').attr('transform', `translate(10, ${years.length * yearHeight + cellSize * 4})`);
-
-const categoriesCount = 10;
-const categories = [...Array(categoriesCount)].map((_, i) => {
-  const upperBound = (maxValue / categoriesCount) * (i + 1);
-  const lowerBound = (maxValue / categoriesCount) * i;
-
-  return {
-    upperBound,
-    lowerBound,
-    color: d3.interpolateBuGn(upperBound / maxValue),
-    selected: true
-  };
-});
-
-fs.writeFileSync('output.svg', d3n.svgString());
 
 // const PDFDocument = require('pdfkit');
 
